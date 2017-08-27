94c0256e3d1d2fa680951907d8591041f7aba6d7
joeflack4 <joeflack4@gmail.com>
2017-08-27 16:32:39 -0400

    Added sorts sub-package.

--- joefuncs/sorts/qsort.py
+++
@@ -1,9 +0,0 @@
-# qsort - A QuickSort Algorithm
-# Author - zangw http://stackoverflow.com/users/3011380/zangw
-# Reference - http://stackoverflow.com/questions/18262306/quick-sort-with-python
-
-def qsort(arr): 
-     if len(arr) <= 1:
-          return arr
-     else:
-          return qsort([x for x in arr[1:] if x<arr[0]]) + [arr[0]] + qsort([x for x in arr[1:] if x>=arr[0]])
