655e58d1b0b942664254ea8787452c21decb2866
joeflack4 <joeflack4@gmail.com>
2017-09-01 12:50:23 -0400

    Added HiddenPrints to io package.

--- joefuncs/io/__init__.py
+++ joefuncs/io/__init__.py
@@ -1,2 +1,21 @@
 """IO."""
 from joefuncs.io import file_io, yaml_io
+
+
+class HiddenPrints:
+    """Allows code to run in a closure without printing to stdout.
+
+    Example:
+        with HiddenPrints():
+            print('This will not appear in stdout.')
+    """
+
+    def __enter__(self):
+        import os
+        import sys
+        self._original_stdout = sys.stdout
+        sys.stdout = open(os.devnull, 'w')
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        import sys
+        sys.stdout = self._original_stdout
