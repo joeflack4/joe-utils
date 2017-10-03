cf09ba7ae69823443ca1b1d3b632df49436985bb
joeflack4 <joeflack4@gmail.com>
2017-10-03 10:21:02 -0400

    Modified setup.py. Added string permutations functions to data_manipulation/string.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
+# Notes
+# * Pypi Classifiers
+#   - https://pypi.python.org/pypi?%3Aaction=list_classifiers
 """Joefuncs - A collection of reusable Python3 functions by Joe Flack
 (joeflack4@gmail.com).
 """
@@ -24,13 +27,15 @@ setup(
     keywords=['joeflack4', 'functions'],
     license='MIT',
     classifiers=[
-        'Development Status :: Alpha',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3.6',
-        'Environment :: Any',
-        'Framework :: None'
+        'Environment :: Other Environment',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: MacOS X',
+        'Environment :: Plugins',
     ]
 )
