937107bb8b47d6ba4c1fb4ec598bda1211deb5c4
joeflack4 <joeflack4@gmail.com>
2018-09-20 11:36:12 -0400

    Added utils for package management.

---
+++ joeutils/package_management/__init__.py
@@ -0,0 +1,8 @@
+"""Package management"""
+# Copy paste this at the top of a file to have it work with with both modulare
+# imports and running as a direct script
+
+# import os
+# import sys
+#
+# sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../')
